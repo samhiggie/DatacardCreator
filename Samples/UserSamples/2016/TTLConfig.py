1daa99fa313a0f989ca7a1a5e6b8b98e7dd0a5fd
Andrew Loeliger <aloelige@cern.ch>
2019-11-07 09:37:11 -0500

    Fixed 2016 TT again

--- Samples/UserSamples/2016/TTLConfig.py
+++ Samples/UserSamples/2016/TTLConfig.py
@@ -12,7 +12,7 @@ from Samples.EventDefinition.UserEventDictionaries.MuTauEventDictionary import M
 TTLSample = Sample()
 TTLSample.name = 'TTL'
 TTLSample.path = '/data/aloeliger/SMHTT_Selected_2016_Deep/'
-TTLSample.files = ['TTTo2L2Nu.root','TTToHadronic.root','TTToSemiLeptonic.root']
+TTLSample.files = ['TT.root']
 TTLSample.definition = 'gen_match_2 < 5'
 TTLSample.uncertainties = [
     TESUncertainty(),
