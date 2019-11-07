6972ee6d444ba6952db7587ae023c85acdb8a8b4
Andrew Loeliger <aloelige@cern.ch>
2019-11-07 09:38:56 -0500

    Complete 2016 splits for embedded and fakes

---
+++ Samples/UserSamples/2016/Embedded_Splits/TTContamination5Config.py
@@ -0,0 +1,19 @@
+from Samples.SampleDefinition import Sample
+
+#
+from Samples.Uncertainties.UserUncertainties.TTbarContamination import TTbarContaminationUncertainty
+
+from Samples.EventDefinition.UserEventDictionaries.MuTauEventDictionary import MuTauEventDictionary
+
+TTContamination = Sample()
+TTContamination.name = 'TTContamination'
+TTContamination.path = '/data/aloeliger/SMHTT_Selected_2016_TTContamination/'
+TTContamination.files = ['TT.root']
+TTContamination.definition = ''
+TTContamination.uncertainties = [
+    TTbarContaminationUncertainty()
+    ]
+TTContamination.eventDictionaryInstance = MuTauEventDictionary
+TTContamination.CreateEventWeight = TTContamination.CreateEventWeight_Standard
+TTContamination.startEntry = 16000
+TTContamination.startEntry = 20000
